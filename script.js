f684915edce717b41388eda02b972ee382c87602
Marshal10 <marshalmaleane123@gmail.com>
2024-04-10 22:12:35 +0530

    Toggle the checkbox icons and text

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const chkBox=document.querySelector('input[type="checkbox"]')
 const nav=document.getElementById('navigation')
 const textBox=document.querySelector('.text-box')
+const toggleIcon=document.querySelector('.toggle-icon')
 
 
 function lightMode(){
     nav.style.backgroundColor='rgb(255,255,255,50%)'
     textBox.style.backgroundColor='rgb(0,0,0,50%)'
+    toggleIcon.children[0].textContent="Light Mode"
+    toggleIcon.children[1].classList.replace('fa-moon','fa-sun')
 }
 
 function darkMode(){
     nav.style.backgroundColor='rgb(0,0,0,50%)'
     textBox.style.backgroundColor='rgb(255,255,255,50%)'
-
+    toggleIcon.children[0].textContent="Dark Mode"
+    toggleIcon.children[1].classList.replace('fa-sun','fa-moon')
 }
 
 function toggleMode(e){
